bf79a1cfff73f2882644207d8236ca691c5d6237
Peter Landry <peter@projectevolution.com>
2012-11-13 19:06:22 -0500

    Add an Open action, and allow start() to be called with no URL

--- unbrowser/casper.py
+++ unbrowser/casper.py
@@ -10,11 +10,17 @@ class Casper(object):
             defaults.update(options)
         self.js = ["var casper = require('casper').create(%s);" % json.dumps(defaults)]
 
-    def start(self, url, action=None):
-        self.js.append("casper.start('%s', function() {" % url)
+    def start(self, url=None, action=None):
+        self.url = 'null'
+        if url is not None:
+            self.url = "'%s'" % url
+        self.js.append("casper.start(%s, function() {" % self.url)
         if action is not None:
             self.js.extend(action.js())
         self.js.append("});")
+
+    def action(self, action):
+        self.js.extend(action.js())
 
     def then(self, action):
         self.js.append("casper.then(function() {")
@@ -34,3 +40,19 @@ class Capture(object):
 
     def js(self):
         return ["this.capture('%s');" % self.filename]
+
+
+class Open(object):
+    def __init__(self, url, settings=None, action=None):
+        self.url = url
+        self.settings = {}
+        if settings is not None:
+            self.settings.update(settings)
+        self._js = ["casper.open('%s', %s" % (self.url, json.dumps(settings), )]
+        self._js.append(").then(function() {")
+        if action is not None:
+            self._js.extend(action.js())
+        self._js.append("});")
+
+    def js(self):
+        return self._js
